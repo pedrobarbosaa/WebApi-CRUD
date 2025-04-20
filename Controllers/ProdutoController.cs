bd7c96bc214dd20fb4d3bb0e11fad206010f5df6
Pedro Henrique de Sousa Barbosa <pedro@appservice.com.br>
2025-04-20 18:31:47 -0300

    :ok_hand:feat: método de edição e exclusão registro

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApi_.NET_9.Data;
 using WebApi_.NET_9.Models;
 
@@ -32,6 +33,60 @@ namespace WebApi_.NET_9.Controllers
                 return NotFound("Registro não encontrado.");
             }
             return Ok(produto); 
+        }
+
+        [HttpPost]
+        public ActionResult<ProdutoModel> CriarProduto(ProdutoModel produtoModel)
+        {
+            if(produtoModel == null)
+            {
+                return BadRequest("Informe os dados do produto");
+            }
+            _context.Produtos.Add(produtoModel);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(BuscarProdutosPorId), new {IdProduto = produtoModel.IdProduto }, produtoModel);
+        }
+
+        [HttpPut("{IdProduto}")]
+        public ActionResult<ProdutoModel> EditarProduto(ProdutoModel produtoModel, int IdProduto)
+        {
+            var produto = _context.Produtos.Find(IdProduto);
+
+            if(produto == null)
+            {
+                return NotFound("Registro não localizado.");
+            }
+
+            produto.NomeProduto = produtoModel.NomeProduto;
+            produto.DescricaoProduto = produtoModel.DescricaoProduto;
+            produto.QuantidadeProduto = produtoModel.QuantidadeProduto;
+            produto.CodBarrasProduto = produtoModel.CodBarrasProduto;
+            produto.MarcaProduto = produtoModel.MarcaProduto;
+
+            _context.Produtos.Update(produto);
+            _context.SaveChanges();
+
+            return Ok("Registro editado com sucesso");
+           
+        }
+
+        [HttpDelete("{IdProduto}")]
+        public ActionResult<ProdutoModel> ExcluirProduto(int IdProduto)
+        {
+
+            var produto = _context.Produtos.Find(IdProduto);
+
+            if(produto == null)
+            {
+                return NotFound("Registro não localizado.");
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok("Registro excluído com sucesso");
+            
         }
     }
 }
